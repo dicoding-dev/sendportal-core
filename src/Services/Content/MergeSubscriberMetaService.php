417ed0dd964f4c8b67b228e3b001fe5a3c961fa8
Rizqy H <rizqy@dicoding.com>
2025-05-22 09:38:38 +0700

    Create service for merging content with subscriber meta

---
+++ src/Services/Content/MergeSubscriberMetaService.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sendportal\Base\Services\Content;
+
+use Sendportal\Base\Models\Subscriber;
+use Sendportal\Base\Traits\NormalizeTags;
+
+class MergeSubscriberMetaService
+{
+    use NormalizeTags;
+
+    private array $tags;
+
+    public function __construct(Subscriber $subscriber)
+    {
+        $this->tags = $subscriber->meta ?? [];
+    }
+
+    public function handle(string $content): string
+    {
+        return $this->mergeTags($content);
+    }
+
+    protected function mergeTags(string $content): string
+    {
+        return $this->mergeMetaTagsValue(
+            $this->compileTags($content)
+        );
+    }
+
+    protected function compileTags(string $content): string
+    {
+        foreach ($this->tags as $tag) {
+            $content = $this->normalizeTags($content, $tag);
+        }
+
+        return $content;
+    }
+
+    protected function mergeMetaTagsValue(string $content): string
+    {
+        foreach ($this->tags as $tag => $value) {
+            $content = str_ireplace('{{' . $tag . '}}', $value, $content);
+        }
+
+        return $content;
+    }
+}
