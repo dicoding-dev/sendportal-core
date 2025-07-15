ecba8b0c550e99b2773bcaa8cef71edd28d5faa8
Rizqy H <rizqy@dicoding.com>
2025-07-15 13:03:15 +0700

    comment out response data temporarily

--- src/Http/Resources/Subscriber.php
+++ src/Http/Resources/Subscriber.php
@@ -20,11 +20,11 @@ class Subscriber extends JsonResource
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'email' => $this->email,
-            'tags' => TagResource::collection($this->whenLoaded('tags')),
-            'meta' => $this->meta,
-            'unsubscribed_at' => $this->unsubscribed_at ? $this->unsubscribed_at->toDateTimeString() : null,
-            'created_at' => $this->created_at->toDateTimeString(),
-            'updated_at' => $this->updated_at->toDateTimeString()
+//            'tags' => TagResource::collection($this->whenLoaded('tags')),
+//            'meta' => $this->meta,
+//            'unsubscribed_at' => $this->unsubscribed_at ? $this->unsubscribed_at->toDateTimeString() : null,
+//            'created_at' => $this->created_at->toDateTimeString(),
+//            'updated_at' => $this->updated_at->toDateTimeString()
         ];
     }
 }
