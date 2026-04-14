37589f6216af2891a51bde158f507c2908ea9505
farida <farida@dicoding.com>
2026-04-14 16:47:48 +0700

    Revert "feat: remove unused `$withCount` property from the `Tag` model."
    This reverts commit 401c122167fb6c2b5778abf3704f3018c79ddfd1.

--- src/Models/Tag.php
+++ src/Models/Tag.php
@@ -42,6 +42,11 @@ class Tag extends BaseModel
         'name',
     ];
 
+    /** @var array */
+    protected $withCount = [
+        'subscribers'
+    ];
+
     public function campaigns(): BelongsToMany
     {
         return $this->belongsToMany(Campaign::class, 'sendportal_campaign_tag');
