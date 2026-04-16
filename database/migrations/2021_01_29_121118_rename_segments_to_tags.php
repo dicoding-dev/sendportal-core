a62d69f689e0e33c4209ac53a0f6728fb5f353ff
farida <farida@dicoding.com>
2026-04-17 01:51:48 +0700

    refactor: update migration to rename indexes and adjust foreign keys

--- database/migrations/2021_01_29_121118_rename_segments_to_tags.php
+++ database/migrations/2021_01_29_121118_rename_segments_to_tags.php
@@ -15,35 +15,43 @@ class RenameSegmentsToTags extends Migration
     {
         Schema::rename('sendportal_segments', 'sendportal_tags');
 
+        Schema::table('sendportal_tags', function (Blueprint $table) {
+            $table->renameIndex('sendportal_segments_workspace_id_index', 'sendportal_tags_workspace_id_index');
+        });
+
         Schema::table('sendportal_segment_subscriber', function (Blueprint $table) {
             $foreignKeys = $this->listTableForeignKeys('sendportal_segment_subscriber');
 
-            if (in_array('sendportal_segment_subscriber_segment_id_foreign', $foreignKeys)) {
+            if (in_array('sendportal_segment_subscriber_segment_id_foreign', $foreignKeys, true)) {
                 $table->dropForeign('sendportal_segment_subscriber_segment_id_foreign');
-            } elseif (in_array('segment_subscriber_segment_id_foreign', $foreignKeys)) {
+            } elseif (in_array('segment_subscriber_segment_id_foreign', $foreignKeys, true)) {
                 $table->dropForeign('segment_subscriber_segment_id_foreign');
             }
 
             $table->renameColumn('segment_id', 'tag_id');
 
-            $table->foreign('tag_id')->references('id')->on('sendportal_tags');
+            $table->renameIndex('sendportal_segment_subscriber_subscriber_id_index', 'sendportal_tag_subscriber_subscriber_id_index');
+
+            $table->foreign('tag_id', 'sendportal_tag_subscriber_tag_id_index')
+                ->references('id')->on('sendportal_tags');
         });
 
         Schema::rename("sendportal_segment_subscriber", "sendportal_tag_subscriber");
 
-
         Schema::table('sendportal_campaign_segment', function (Blueprint $table) {
             $foreignKeys = $this->listTableForeignKeys('sendportal_campaign_segment');
 
-            if (in_array('sendportal_campaign_segment_segment_id_foreign', $foreignKeys)) {
+            if (in_array('sendportal_campaign_segment_segment_id_foreign', $foreignKeys, true)) {
                 $table->dropForeign('sendportal_campaign_segment_segment_id_foreign');
-            } elseif (in_array('campaign_segment_segment_id_foreign', $foreignKeys)) {
+            } elseif (in_array('campaign_segment_segment_id_foreign', $foreignKeys, true)) {
                 $table->dropForeign('campaign_segment_segment_id_foreign');
             }
 
             $table->renameColumn('segment_id', 'tag_id');
 
-            $table->foreign('tag_id')->references('id')->on('sendportal_tags');
+            $table->renameIndex('sendportal_campaign_segment_campaign_id_index', 'sendportal_campaign_tag_campaign_id_index');
+
+            $table->foreign('tag_id', 'sendportal_campaign_tag_tag_id_index')->references('id')->on('sendportal_tags');
         });
 
         Schema::rename("sendportal_campaign_segment", "sendportal_campaign_tag");
